79147e2efb7f77bef0476eeb5d83f7e0a7d7eb96
Maxwell G <maxwell@gtmx.me>
2024-03-03 21:47:14 +0000

    tests: use local go_vendor_archive if available

--- contrib/integration-archive.sh
+++ contrib/integration-archive.sh
@@ -4,10 +4,18 @@
 
 set -euo pipefail
 
+path="$(command -v go_vendor_archive 2>/dev/null || :)"
+command=()
+if [ -n "${path}" ]; then
+    command=("${path}")
+else
+    command=("pipx" "run" "--spec" "../../../" "go_vendor_archive")
+fi
+
+
 spectool -g ./*.spec
 ls
 source0="$(spectool ./*.spec | grep Source0 | awk '{print $2}' | xargs -d'\n' basename)"
 source1="$(spectool ./*.spec | grep Source1 | awk '{print $2}')"
-pipx run --spec ../../../ go_vendor_archive \
-    -O "${source1}" "$@" "${source0}"
+"${command[@]}" -O "${source1}" "$@" "${source0}"
 sha512sum -c CHECKSUMS
