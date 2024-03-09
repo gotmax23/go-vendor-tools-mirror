a18a43159f2b6b565581013290af660ad2748141
Maxwell G <maxwell@gtmx.me>
2024-03-09 03:08:05 +0000

    tests integration: check go_vendor_license stdout

--- contrib/verify-license.sh
+++ contrib/verify-license.sh
@@ -17,3 +17,6 @@ if [ -f "go-vendor-tools.toml" ]; then
     command+=("--config" "$(pwd)/go-vendor-tools.toml")
 fi
 "${command[@]}" -C ./*.spec report all --verify "${license}"
+# Ensure the stdout is also correct
+simplified="$(python -c 'from go_vendor_tools.licensing import simplify_license; import sys; print(simplify_license(sys.argv[1]))' "${license}")"
+[ "$("${command[@]}" -C ./*.spec report expression)" = "${simplified}" ]
