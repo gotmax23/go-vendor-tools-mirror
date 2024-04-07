b5f16e7bfa0f00ab27b1cd918a7cd201f14c80ee
Maxwell G <maxwell@gtmx.me>
2024-04-07 22:26:08 +0000

    tests: cover `go_vendor_archive create --idempotent`

--- contrib/integration-archive.sh
+++ contrib/integration-archive.sh
@@ -10,7 +10,7 @@ _path="$(command -v go_vendor_archive 2>/dev/null || :)"
 _default_path="pipx run --spec ../../../ go_vendor_archive"
 GO_VENDOR_ARCHIVE="${GO_VENDOR_ARCHIVE:-${_path:-${_default_path}}}"
 IFS=" " read -r -a command <<< "${GO_VENDOR_ARCHIVE}"
-command+=("create")
+command+=("create" "--idempotent")
 
 spectool -g ./*.spec
 ls
@@ -18,4 +18,7 @@ if [ -f "go-vendor-tools.toml" ]; then
     command+=("--config" "$(pwd)/go-vendor-tools.toml")
 fi
 time "${command[@]}" "$@" ./*.spec
+# Test idempotency by running again with networking disabled and a timeout
+# to make sure nothing is downloaded.
+timeout 5 unshare -rn "${command[@]}" "$@" ./*.spec | grep 'already exists'
 sha512sum -c CHECKSUMS
