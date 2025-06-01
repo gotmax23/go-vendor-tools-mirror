gotmax23/go-vendor-tools-mirror

commit 6790105a14787a8edbb2e394efa2aefab31d488d

license_detection: document backends and make askalono default
Fixes: https://gitlab.com/fedora/sigs/go/go-vendor-tools/-/issues/57

src/go_vendor_tools/license_detection/load.py
@@ -15,9 +15,8 @@ from .trivy import TrivyLicenseDetector
 
 DETECTORS: dict[str, type[LicenseDetector]] = {
     TrivyLicenseDetector.NAME: TrivyLicenseDetector,
-    AskalonoLicenseDetector.NAME: AskalonoLicenseDetector,
-    # TODO(gotmax23): Make scancode the default
     ScancodeLicenseDetector.NAME: ScancodeLicenseDetector,
+    AskalonoLicenseDetector.NAME: AskalonoLicenseDetector,
 }
 
 

commit 21413ae05758b7ccc396913e8ca89d21e7e471cf

license_detection: actually make askalono the default
Fixes: 6790105a14787a8edbb2e394efa2aefab31d488d

src/go_vendor_tools/license_detection/load.py
@@ -14,9 +14,9 @@ from .scancode import ScancodeLicenseDetector
 from .trivy import TrivyLicenseDetector
 
 DETECTORS: dict[str, type[LicenseDetector]] = {
-    TrivyLicenseDetector.NAME: TrivyLicenseDetector,
-    ScancodeLicenseDetector.NAME: ScancodeLicenseDetector,
     AskalonoLicenseDetector.NAME: AskalonoLicenseDetector,
+    ScancodeLicenseDetector.NAME: ScancodeLicenseDetector,
+    TrivyLicenseDetector.NAME: TrivyLicenseDetector,
 }
 
 