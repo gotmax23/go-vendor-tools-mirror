66d7d214e8dd4b75669cf765ff173a57fa6b3e15
Maxwell G <maxwell@gtmx.me>
2025-08-30 22:41:08 -0500

    gocheck2: fix -skip handling
    I misunderstood the way go test handles -skip.

--- tests/integration/fzf/test_test.go
+++ tests/integration/fzf/test_test.go
@@ -7,3 +7,7 @@ import "testing"
 func TestBroken(t *testing.T) {
 	t.Error("This test is supposed to fail.")
 }
+
+func TestSomethingElseBroken(t *testing.T) {
+	t.Error("This test is also supposed to fail.")
+}
