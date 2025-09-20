gotmax23/go-vendor-tools-mirror

commit 4c11a70685f3b244f489a78569982056af59e883

license: allow --autofill when --prompt is not passed
We should still call `fill_missing_licenses()` even if `prompt` is disabled.
The rest of the code was changed to allow either or both options to be
passed (or neither to be passed — in which case, `fill_missing_licenses`
isn't called at all, as this block would still evaluate to False), but
this condition was still broken.

src/go_vendor_tools/cli/go_vendor_license.py
@@ -710,7 +710,7 @@ def report_command(args: argparse.Namespace) -> None:
                 go_mod_dir,
             )
         )
-        if prompt:
+        if prompt or autofill_detector:
             license_data = fill_missing_licenses(
                 license_data, loaded, autofill_detector, prompt
             )

commit e96733cbedcb3c801ab24c65badf97a821b493fc

license: imply --write-config when --autofill is enabled
Fixes: 4c11a70685f3b244f489a78569982056af59e883

src/go_vendor_tools/cli/go_vendor_license.py
@@ -666,7 +666,9 @@ def report_command(args: argparse.Namespace) -> None:
     mode: str = args.mode
     verify: str | None = args.verify
     write_json: Path | None = args.write_json
-    write_config: bool = args.write_config or args.prompt
+    write_config: bool = (
+        args.write_config or args.prompt or autofill_detector is not None
+    )
     prompt: bool = args.prompt
     config_path: Path | None = args.config_path
     use_archive: bool = args.use_archive