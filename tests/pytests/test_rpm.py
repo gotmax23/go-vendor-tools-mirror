56bb8d9c9005707bd8fd4f8e850097cbc5b2e466
Maxwell G <maxwell@gtmx.me>
2025-10-16 20:34:52 -0500

    Automatically install vendor/modules.txt with %go_vendor_license_install
    Fixes: https://gitlab.com/fedora/sigs/go/go-vendor-tools/-/issues/83

--- tests/pytests/test_rpm.py
+++ tests/pytests/test_rpm.py
@@ -73,6 +73,20 @@ class Evaluator:
 evaluator = Evaluator()
 
 
+def test_go_vendor_license_install():
+    assert (
+        evaluator("%go_vendor_license_install", defines={"NAME": "foo"}).stdout
+        == "go_vendor_license install --destdir %{buildroot} --install-directory /usr/share/licenses/foo --filelist licenses.list\n"  # noqa: E501
+    )
+
+
+def test_go_vendor_license_install_M():
+    assert (
+        evaluator("%go_vendor_license_install -M", defines={"NAME": "foo"}).stdout
+        == "go_vendor_license install --destdir %{buildroot} --install-directory /usr/share/licenses/foo --filelist licenses.list -M\n"  # noqa: E501
+    )
+
+
 def test_go_vendor_license_check_disabled():
     assert not (
         evaluator(
